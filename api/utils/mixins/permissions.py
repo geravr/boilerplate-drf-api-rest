6ee8992f3faee37eb74a5f230d4b5ee7c48bb7eb
Gerardo Villa Ramírez <gerardovr@MacBook-Pro.local>
2020-10-01 22:52:53 -0500

    update permission for CustomDjangoModelPermission

--- api/utils/mixins/permissions.py
+++ api/utils/mixins/permissions.py
@@ -1,6 +1,8 @@
 # Rest Framework
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from authentication.permissions import CustomDjangoModelPermission
 
 
 class DefaultPermissionsMixin(object):
-      permission_classes = [AllowAny]
+      permission_classes = [CustomDjangoModelPermission]
+
